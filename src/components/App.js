611b9048a296a5af7fd6cecd3d8fa2bff8a98da0
ltien <loctientan@gmail.com>
2021-08-14 17:54:41 +0700

    create app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+// import Dashboard from './Dashboard/Dashboard';
+import Login from './Login/Login';
+
+function App() {
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
